2ccb6c398365a20719b0f7e6df974624df456158
Alejandro <alexlisti15@gmail.com>
2025-10-29 18:42:48 +0100

    script 1 creo vpc

--- script1.sh
+++ script1.sh
@@ -1,6 +1,4 @@
 aws ec2 create-vpc \
     --cidr-block 192.168.0.0/24 \
     --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=VPCALEX}]' \
-    --query Vpc.VpcId --output text
-
-    
+    --query Vpc.VpcId --output text aa
