81e6496ad14f68d2ce4bdfc98e6de266fb60dc03
Alejandro <alexlisti15@gmail.com>
2025-10-29 19:19:30 +0100

    script 1 vpc funcional con dns

--- script1.sh
+++ script1.sh
@@ -1,7 +1,13 @@
 
-#Creo la vpc y devuelvo su id
+    #Creo la vpc y devuelvo su id
+    VPC_ID=$(aws ec2 create-vpc \
+        --cidr-block 192.168.1.0/24 \
+        --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=VPCALEX}]' \
+        --query Vpc.VpcId --output text) 
 
-VPC_ID=$(aws ec2 create-vpc \
-    --cidr-block 192.168.0.0/24 \
-    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=VPCALEX}]' \
-    --query Vpc.VpcId --output text) 
+    #Muestro el id de la vpc
+    echo $VPC_ID
+    #habilitar dns en la vpc
+    aws ec2 modify-vpc-attribute \
+        --vpc-id $VPC_ID \
+        --enable-dns-hostnames "{\"Value\":true}"
