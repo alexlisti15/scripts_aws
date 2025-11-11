2dc3c38c3ea4b012d7051379c310959d4c0b6150
Alejandro <alexlisti15@gmail.com>
2025-11-11 19:56:23 +0100

    script 1 vpc+subred+gs+natgateway+routetables

--- script3.sh
+++ script3.sh
@@ -1,8 +1,10 @@
 
-    #Creo la vpc y devuelvo su id
+
+
+#Creo la vpc y devuelvo su id
     VPC_ID=$(aws ec2 create-vpc \
-        --cidr-block 192.168.0.0/24 \
-        --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=VPCALEX3}]' \
+        --cidr-block 172.16.0.0/16 \
+        --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=VPCALEX6}]' \
         --query Vpc.VpcId --output text) 
 
     #Muestro el id de la vpc
@@ -14,8 +16,8 @@
 
     SUB_ID=$(aws ec2 create-subnet \
         --vpc-id $VPC_ID \
-        --cidr-block 192.168.0.1/28 \
-        --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=Subred2-Alex}]' \
+        --cidr-block 172.16.0.0/20 \
+        --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=Subred6-Alex}]' \
         --query Subnet.SubnetId --output text)
 
         echo $SUB_ID 
@@ -24,9 +26,12 @@
     #Comprobar como no se hailita y tendremos que hacerlo a posteriori
     aws ec2 modify-subnet-attribute --subnet-id $SUB_ID --map-public-ip-on-launch
 
-    #Crear grupo Security Group 
+
+
+
+      #Crear grupo Security Group 
     SG_ID=$(aws ec2 create-security-group \
-    --group-name sgmio7 \
+    --group-name sgmio4 \
     --description "Mi grupo de seguridad para abrir el puerto 22" \
     --vpc-id $VPC_ID \
     --query GroupId --output text )
@@ -35,13 +40,11 @@
 
 # Autorizar el tráfico SSH (puerto 22) desde cualquier IP
 aws ec2 authorize-security-group-ingress \
-  --group-id "$SG_ID" \
+  --group-id $SG_ID \
   --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "0.0.0.0/0", "Description": "All"}]}]'
 
-
-
     IGW_ID=$(aws ec2 create-internet-gateway \
-        --tag-specifications 'ResourceType=internet-gateway,Tags=[{Key=Name,Value=my-igwalex8}]' \
+        --tag-specifications 'ResourceType=internet-gateway,Tags=[{Key=Name,Value=my-igwalex4}]' \
         --query InternetGateway.InternetGatewayId --output text)
 
     aws ec2 attach-internet-gateway \
@@ -58,26 +61,3 @@ aws ec2 authorize-security-group-ingress \
     
 
 
-
-# Crear una instancia EC2 en la subred especificada
-EC2_ID=$(aws ec2 run-instances \
-  --image-id ami-0360c520857e3138f \
-  --instance-type t3.micro \
-  --subnet-id "$SUB_ID" \
-  --security-group-ids $SG_ID \
-  --key-name vockey \
-  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MiEC4}]' \
-  --associate-public-ip-address \
-  --query 'Instances[0].InstanceId' \
-  --output text)
-
-
-
-# Esperar unos segundos para que la instancia se inicialice
-sleep 15
-
-# Mostrar el ID de la instancia creada
-echo "EC2 Instance ID: $EC2_ID"
-        
-
-    
